c647d55d869643a02e5903429c25798c661f175f
Alphaeus Ng <78588510+Alphatortoise@users.noreply.github.com>
2022-03-23 15:09:41 +0800

    Update MedicalInfo.js
    added JSdoc

--- JS_files/Version 2.0/Screens/MedicalInfo.js
+++ JS_files/Version 2.0/Screens/MedicalInfo.js
@@ -1,7 +1,11 @@
-// change in your manifest file: (android:windowSoftInputMode="adjustPan") for keyboardavoidingview to work
+/** External changes to make:
+ * change in your manifest file: (android:windowSoftInputMode="adjustPan") for keyboardavoidingview to work*/
+
 import React from 'react';
 //import { NavigationContainer } from '@react-navigation/native';
 //import { useNavigation } from '@react-navigation/native';
+
+// Import relevant tools from react-native
 import {
   Dimensions,
   StyleSheet,
@@ -16,39 +20,77 @@ import {
   Platform,
   ScrollView,
 } from 'react-native';
+
+// Ensure that input box are dynamically updated to screen size
 const SCREEN_WIDTH = Dimensions.get('screen').width - 30;
+
+// Constant value for easy change of all the input boxes
 const BORDER_BOTTOM_WIDTH_CONST = 1;
+
+// Constant value for first text location, subsequent texts are positioned according to this cornerstone text
 const FIRSTTEXT_CONST = 230;
+
+// Constant different values for easy change of all input boxes
 const DIFFERENCE_CONST = 40;
+
+// Constant color value for easy change of color scheme
 const COLOR_CONST = 'aliceblue';
 
+/**
+ * Default function for MedicalInfo Screen
+ * @return {MedicalInfoUI} The User Interface screen to be displayed
+ */
 export default function MedicalInfoUI() {
+
+  // Declare a const function, to save the information keyed in the MedicalInfo UI input boxes
   const save = () =>{
     // to be updated
   }
+
+  // Declare a const function, upon pressing the back button, return to main menu screen
   const back = () =>{
     // navigation.navigate("Main Menu");
   }
+  // Declare a new state variable, for changing Blood Type
   const [inputBloodType_text, onChangeBloodType_text] = React.useState(null);
+
+  // Declare a new state variable, for changing Heart Condition
   const [inputHeartCondition_text, onChangeHeartCondition_text] =
     React.useState(null);
+
+  // Declare a new state variable, for changing Asthma Condition
   const [inputAsthma_text, onChangeAsthma_text] = React.useState(null);
+
+  // Declare a new state variable, for changing Allergy Condition
   const [inputAllergy_text, onChangeAllergy_text] = React.useState(null);
+
+  // Declare a new state variable, for changing Drug Allergy Condition
   const [inputDrugAllergiesy_text, onChangeDrugAllergies_text] =
     React.useState(null);
+
+  // Declare a new state variable, for changing Health Condition Details
   const [inputHealthConditionDetails_text, onChangeHeartConditionDetails_text] =
     React.useState(null);
+
+  // Declare a new state variable, for changing Health Conditions
   const [inputotherHealthCondition_text, onChangeotherHealthCondition_text] =
     React.useState(null);
 
   return (
+    // KeyboardAvoidingView to prevent input boxes from being blocked when keyboard comes up
     <KeyboardAvoidingView
       style = {styles.container}
       behavior="padding"
-      >
+    >
+      // TouchableOpacity to highlight a 'touch' feel when back button is being pressed
       <TouchableOpacity style={styles.backButton}>
+
+        // To retrieve icon.png from assets folder
         <Image source={require('./assets/backButtonIcon.png')} onPress={() => back}/>
+
       </TouchableOpacity>
+
+      // TextInput for user to enter their blood type
       <TextInput
         style={styles.inputBloodType}
         textAlign="center"
@@ -56,8 +98,8 @@ export default function MedicalInfoUI() {
         keyboardType="alphanumeric"
         onChangeBloodType_text={onChangeBloodType_text}
         value={inputBloodType_text}
-        
       />
+      // TextInput for user to enter their allergies
       <TextInput
         style={styles.inputAllergies}
         textAlign="left"
@@ -66,6 +108,7 @@ export default function MedicalInfoUI() {
         onChangeAllergy_text={onChangeAllergy_text}
         value={inputAllergy_text}
       />
+      // TextInput for user to enter their drug allergies
       <TextInput
         style={styles.inputDrugAllergies}
         textAlign="left"
@@ -74,7 +117,7 @@ export default function MedicalInfoUI() {
         onChangeDrugAllergies_text={onChangeDrugAllergies_text}
         value={inputDrugAllergiesy_text}
       />
-
+      // TextInput for user to enter their heart condition
       <TextInput
         style={styles.inputHeartCondition}
         textAlign="center"
@@ -83,7 +126,9 @@ export default function MedicalInfoUI() {
         placeholder="No"
         keyboardType="alphanumeric"
       />
+      // Text style for Asthma text
       <Text style={styles.asthmaSubText}>Asthma</Text>
+      // TextInput for user to enter their asthma condition
       <TextInput
         style={styles.inputAsthma}
         textAlign="center"
@@ -92,6 +137,7 @@ export default function MedicalInfoUI() {
         placeholder="No"
         keyboardType="alphanumeric"
       />
+      // TextInput for user to enter their heart condition details
       <TextInput
         style={styles.inputHeartConditionDetails}
         textAlign="left"
@@ -100,6 +146,7 @@ export default function MedicalInfoUI() {
         placeholder="Heart condition details"
         keyboardType="alphanumeric"
       />
+      // TextInput for user to enter other heart conditions
       <TextInput
         style={styles.otherHealthCondition}
         textAlign="left"
@@ -108,9 +155,16 @@ export default function MedicalInfoUI() {
         placeholder="Other Health Conditions"
         keyboardType="alphanumeric"
       />
+      // Text style to display Medical Info title text
       <Text style={styles.titleText}>Medical Info</Text>
+
+      // Text style to display Blood Type sub text
       <Text style={styles.bloodTypeSubText}>Blood Type</Text>
+      
+      // Text style to display Heart Condition sub text
       <Text style={styles.heartConditionSubText}>Heart Condition</Text>
+
+      // View style to display save button container (uses default icon provided by Android/IOS)
       <View style={styles.saveButtonContainer}>
         <Button title="Save" onPress={() => save} />
       </View>
@@ -118,12 +172,16 @@ export default function MedicalInfoUI() {
   );
 }
 
+// Store different const styles for creation of different items
 const styles = StyleSheet.create({
+  // General container for whole screen
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
   },
+
+  // backButton style
   backButton: {
     flex: 1,
     resizeMode: 'contain',
@@ -131,6 +189,8 @@ const styles = StyleSheet.create({
     left: 20,
     position: 'absolute',
   },
+
+  // titleText style (Text output for MedicalInfo)
   titleText: {
     fontSize: 40,
     fontWeight: 'bold',
@@ -139,6 +199,8 @@ const styles = StyleSheet.create({
     position: 'absolute',
     top: FIRSTTEXT_CONST - 120,
   },
+
+  // bloodTypeSubText style (Text output for Blood Type)
   bloodTypeSubText: {
     fontSize: 23,
     fontWeight: 'normal',
@@ -146,6 +208,8 @@ const styles = StyleSheet.create({
     left: 25,
     top: FIRSTTEXT_CONST,
   },
+
+  // inputBloodType style (Text input for blood type)
   inputBloodType: {
     backgroundColor: COLOR_CONST,
     paddingHorizontal: 10,
@@ -157,6 +221,8 @@ const styles = StyleSheet.create({
     right: 15,
     borderBottomWidth: BORDER_BOTTOM_WIDTH_CONST,
   },
+
+  // inputAllergies style (Text input for allergies)
   inputAllergies: {
     backgroundColor: 'white',
     color: 'grey',
@@ -172,6 +238,8 @@ const styles = StyleSheet.create({
     flex: 1,
     borderBottomWidth: BORDER_BOTTOM_WIDTH_CONST,
   },
+
+  // inputDrugAllergies (Text input for drug allergies)
   inputDrugAllergies: {
     backgroundColor: 'white',
     color: 'grey',
@@ -186,6 +254,8 @@ const styles = StyleSheet.create({
     fontSize: 20,
     borderBottomWidth: BORDER_BOTTOM_WIDTH_CONST,
   },
+
+  // heartConditionSubText (Text output for Heart Condition)
   heartConditionSubText: {
     fontSize: 22,
     fontWeight: 'normal',
@@ -193,6 +263,8 @@ const styles = StyleSheet.create({
     left: 25,
     top: FIRSTTEXT_CONST + DIFFERENCE_CONST * 4.5,
   },
+
+  // heartCondition (Text input for Heart Condition)
   inputHeartCondition: {
     backgroundColor: COLOR_CONST,
     paddingHorizontal: 25,
@@ -202,9 +274,10 @@ const styles = StyleSheet.create({
     position: 'absolute',
     top: FIRSTTEXT_CONST + DIFFERENCE_CONST * 4.5 - 12,
     right: 20,
-
     borderBottomWidth: BORDER_BOTTOM_WIDTH_CONST,
   },
+  
+  // inputHeartConditionDetails (Text input for Heart Condition)
   inputHeartConditionDetails: {
     backgroundColor: 'white',
     width: SCREEN_WIDTH,
@@ -218,6 +291,8 @@ const styles = StyleSheet.create({
     fontSize: 20,
     borderBottomWidth: BORDER_BOTTOM_WIDTH_CONST,
   },
+
+  // asthmaSubText (Text output for Asthma)
   asthmaSubText: {
     fontSize: 23,
     fontWeight: 'normal',
@@ -225,6 +300,8 @@ const styles = StyleSheet.create({
     left: 25,
     top: FIRSTTEXT_CONST + DIFFERENCE_CONST * 7.5,
   },
+
+  // inputAsthma (Text input for Asthma)
   inputAsthma: {
     backgroundColor: COLOR_CONST,
     paddingHorizontal: 25,
@@ -236,6 +313,8 @@ const styles = StyleSheet.create({
     right: 20,
     borderBottomWidth: BORDER_BOTTOM_WIDTH_CONST,
   },
+
+  // otherHealthCondition (Text output for Other Health Condition)
   otherHealthCondition: {
     backgroundColor: 'white',
     color: 'grey',
@@ -250,6 +329,8 @@ const styles = StyleSheet.create({
     fontSize: 20,
     borderBottomWidth: BORDER_BOTTOM_WIDTH_CONST,
   },
+
+  // saveButtonContainer style (save button)
   saveButtonContainer: {
     width: '20%',
     position: 'absolute',
