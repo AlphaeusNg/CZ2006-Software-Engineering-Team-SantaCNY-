5f7fdce33054333a3dd0e5a0a0df3a152de8c214
Alphaeus Ng <78588510+Alphatortoise@users.noreply.github.com>
2022-03-23 14:39:08 +0800

    Update MedicalInfo.js

--- JS_files/Version 2.0/Screens/MedicalInfo.js
+++ JS_files/Version 2.0/Screens/MedicalInfo.js
@@ -1,4 +1,7 @@
+// change in your manifest file: (android:windowSoftInputMode="adjustPan") for keyboardavoidingview to work
 import React from 'react';
+//import { NavigationContainer } from '@react-navigation/native';
+//import { useNavigation } from '@react-navigation/native';
 import {
   Dimensions,
   StyleSheet,
@@ -9,87 +12,109 @@ import {
   SafeAreaView,
   Button,
   TextInput,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
 } from 'react-native';
 const SCREEN_WIDTH = Dimensions.get('screen').width - 30;
 const BORDER_BOTTOM_WIDTH_CONST = 1;
 const FIRSTTEXT_CONST = 230;
 const DIFFERENCE_CONST = 40;
-const COLOR_CONST = 'aliceblue'; 
+const COLOR_CONST = 'aliceblue';
+
 export default function MedicalInfoUI() {
+  const save = () =>{
+    // to be updated
+  }
+  const back = () =>{
+    // navigation.navigate("Main Menu");
+  }
   const [inputBloodType_text, onChangeBloodType_text] = React.useState(null);
-  const [inputHeartCondition_text, onChangeHeartCondition_text] = React.useState(null);
+  const [inputHeartCondition_text, onChangeHeartCondition_text] =
+    React.useState(null);
   const [inputAsthma_text, onChangeAsthma_text] = React.useState(null);
   const [inputAllergy_text, onChangeAllergy_text] = React.useState(null);
-  const [inputDrugAllergiesy_text, onChangeDrugAllergies_text] = React.useState(null);
-  const [inputHealthConditionDetails_text, onChangeHeartConditionDetails_text] = React.useState(null);
-  const [inputotherHealthCondition_text, onChangeotherHealthCondition_text] = React.useState(null);
-  
+  const [inputDrugAllergiesy_text, onChangeDrugAllergies_text] =
+    React.useState(null);
+  const [inputHealthConditionDetails_text, onChangeHeartConditionDetails_text] =
+    React.useState(null);
+  const [inputotherHealthCondition_text, onChangeotherHealthCondition_text] =
+    React.useState(null);
 
   return (
-      <SafeAreaView style={styles.container}>
+    <KeyboardAvoidingView
+      style = {styles.container}
+      behavior="padding"
+      >
+      <TouchableOpacity style={styles.backButton}>
+        <Image source={require('./assets/backButtonIcon.png')} onPress={() => back}/>
+      </TouchableOpacity>
       <TextInput
-          style={styles.inputBloodType} textAlign='right'
-          placeholder="Enter your blood type"
-          keyboardType="alphanumeric"
-          onChangeBloodType_text={onChangeBloodType_text}
-          value={inputBloodType_text}          
-        />
+        style={styles.inputBloodType}
+        textAlign="center"
+        placeholder="Enter your blood type"
+        keyboardType="alphanumeric"
+        onChangeBloodType_text={onChangeBloodType_text}
+        value={inputBloodType_text}
+        
+      />
       <TextInput
-        style={styles.inputAllergies} textAlign='left'
+        style={styles.inputAllergies}
+        textAlign="left"
         placeholder="Allergies"
-        keyboardType='alphanumeric'
+        keyboardType="alphanumeric"
         onChangeAllergy_text={onChangeAllergy_text}
         value={inputAllergy_text}
-        />
-        <TextInput
-        style={styles.inputDrugAllergies} textAlign='left'
+      />
+      <TextInput
+        style={styles.inputDrugAllergies}
+        textAlign="left"
         placeholder="Drug Allergies"
-        keyboardType='alphanumeric'
+        keyboardType="alphanumeric"
         onChangeDrugAllergies_text={onChangeDrugAllergies_text}
         value={inputDrugAllergiesy_text}
-        />
+      />
 
-        <TextInput
-          style={styles.inputHeartCondition} textAlign='right'
-          onChangeHeartCondition_text={onChangeHeartCondition_text}
-          value={inputHeartCondition_text}
-          placeholder="No"
-          keyboardType="alphanumeric"
-        />
-        <TextInput
-          style={styles.inputAsthma} textAlign='right'
-          onChangeAsthma_text={onChangeAsthma_text}
-          value={inputAsthma_text}
-          placeholder="No"
-          keyboardType="alphanumeric"
-        />
-        <TextInput
-          style={styles.inputHeartConditionDetails} textAlign='left'
-          onChangeHeartConditionDetails_text={onChangeHeartConditionDetails_text}
-          value={inputHealthConditionDetails_text}
-          placeholder="Heart condition details"
-          keyboardType="alphanumeric"
-        />
-        <TextInput
-          style={styles.otherHealthCondition} textAlign='left'
-          onChangeotherHealthCondition_text={onChangeotherHealthCondition_text}
-          value={inputotherHealthCondition_text}
-          placeholder="Other Health Conditions"
-          keyboardType="alphanumeric"
-        />
-        <Text style={styles.titleText}>Medical Info</Text>
-        <Text style={styles.bloodTypeSubText}>Blood Type</Text>
-        <Text style={styles.heartConditionSubText}>Heart Condition</Text>
-        <Text style={styles.asthmaSubText}>Asthma</Text>
-        <TouchableOpacity style={styles.backButton}>
-          <Image source={require('./assets/backButtonIcon.png')} />
-        </TouchableOpacity>
-        <View style = {styles.saveButtonContainer}>
-          <TouchableOpacity style = {[styles.button,styles.buttonOutline]}>
-            <Text style ={styles.buttonOutlineText}>Save</Text>
-          </TouchableOpacity>
-        </View>
-      </SafeAreaView>
+      <TextInput
+        style={styles.inputHeartCondition}
+        textAlign="center"
+        onChangeHeartCondition_text={onChangeHeartCondition_text}
+        value={inputHeartCondition_text}
+        placeholder="No"
+        keyboardType="alphanumeric"
+      />
+      <Text style={styles.asthmaSubText}>Asthma</Text>
+      <TextInput
+        style={styles.inputAsthma}
+        textAlign="center"
+        onChangeAsthma_text={onChangeAsthma_text}
+        value={inputAsthma_text}
+        placeholder="No"
+        keyboardType="alphanumeric"
+      />
+      <TextInput
+        style={styles.inputHeartConditionDetails}
+        textAlign="left"
+        onChangeHeartConditionDetails_text={onChangeHeartConditionDetails_text}
+        value={inputHealthConditionDetails_text}
+        placeholder="Heart condition details"
+        keyboardType="alphanumeric"
+      />
+      <TextInput
+        style={styles.otherHealthCondition}
+        textAlign="left"
+        onChangeotherHealthCondition_text={onChangeotherHealthCondition_text}
+        value={inputotherHealthCondition_text}
+        placeholder="Other Health Conditions"
+        keyboardType="alphanumeric"
+      />
+      <Text style={styles.titleText}>Medical Info</Text>
+      <Text style={styles.bloodTypeSubText}>Blood Type</Text>
+      <Text style={styles.heartConditionSubText}>Heart Condition</Text>
+      <View style={styles.saveButtonContainer}>
+        <Button title="Save" onPress={() => save} />
+      </View>
+    </KeyboardAvoidingView>   
   );
 }
 
@@ -99,10 +124,8 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
-  backButton: { 
+  backButton: {
     flex: 1,
-    width: null,
-    height: null,
     resizeMode: 'contain',
     top: 50,
     left: 20,
@@ -110,141 +133,127 @@ const styles = StyleSheet.create({
   },
   titleText: {
     fontSize: 40,
-    fontWeight: 'italic',
-    position: "absolute",
-    top: FIRSTTEXT_CONST-120,
+    fontWeight: 'bold',
+    fontStyle: 'italic',
+    color: 'darkblue',
+    position: 'absolute',
+    top: FIRSTTEXT_CONST - 120,
   },
   bloodTypeSubText: {
     fontSize: 23,
-    fontWeight: "normal",
+    fontWeight: 'normal',
     position: 'absolute',
     left: 25,
     top: FIRSTTEXT_CONST,
   },
-  inputBloodType:{
-        backgroundColor: COLOR_CONST,
-        paddingHorizontal:25,
-        paddingVertical:7,
-        borderRadius:20,
-        marginTop:5,
-        position: 'absolute',
-        top: FIRSTTEXT_CONST-10,
-        right: 15,
-        borderBottomWidth: BORDER_BOTTOM_WIDTH_CONST,
+  inputBloodType: {
+    backgroundColor: COLOR_CONST,
+    paddingHorizontal: 10,
+    paddingVertical: 7,
+    borderRadius: 20,
+    marginTop: 5,
+    position: 'absolute',
+    top: FIRSTTEXT_CONST - 10,
+    right: 15,
+    borderBottomWidth: BORDER_BOTTOM_WIDTH_CONST,
   },
-  inputAllergies:{
-        backgroundColor:'white',
-        color: 'grey',
-        width: SCREEN_WIDTH,
-        paddingHorizontal:10,
-        paddingVertical:10,
-        borderRadius:20,
-        marginTop:5,
-        position: 'absolute',
-        top: FIRSTTEXT_CONST + DIFFERENCE_CONST,
-        left: 15,
-        fontSize: 20,
-        flex: 1,
-        borderBottomWidth: BORDER_BOTTOM_WIDTH_CONST,
+  inputAllergies: {
+    backgroundColor: 'white',
+    color: 'grey',
+    width: SCREEN_WIDTH,
+    paddingHorizontal: 10,
+    paddingVertical: 10,
+    borderRadius: 20,
+    marginTop: 5,
+    position: 'absolute',
+    top: FIRSTTEXT_CONST + DIFFERENCE_CONST,
+    left: 15,
+    fontSize: 20,
+    flex: 1,
+    borderBottomWidth: BORDER_BOTTOM_WIDTH_CONST,
   },
-  inputDrugAllergies:{
-        backgroundColor:'white',
-        color: 'grey',
-        width: SCREEN_WIDTH,
-        paddingHorizontal:10,
-        paddingVertical:10,
-        borderRadius:20,
-        marginTop:5,
-        position: 'absolute',
-        top: FIRSTTEXT_CONST + DIFFERENCE_CONST*2.5,
-        left: 15,
-        fontSize: 20,
-        borderBottomWidth: BORDER_BOTTOM_WIDTH_CONST,
+  inputDrugAllergies: {
+    backgroundColor: 'white',
+    color: 'grey',
+    width: SCREEN_WIDTH,
+    paddingHorizontal: 10,
+    paddingVertical: 10,
+    borderRadius: 20,
+    marginTop: 5,
+    position: 'absolute',
+    top: FIRSTTEXT_CONST + DIFFERENCE_CONST * 2.5,
+    left: 15,
+    fontSize: 20,
+    borderBottomWidth: BORDER_BOTTOM_WIDTH_CONST,
   },
   heartConditionSubText: {
     fontSize: 22,
-    fontWeight: "normal",
+    fontWeight: 'normal',
     position: 'absolute',
     left: 25,
-    top: FIRSTTEXT_CONST + DIFFERENCE_CONST*4.5,
+    top: FIRSTTEXT_CONST + DIFFERENCE_CONST * 4.5,
+  },
+  inputHeartCondition: {
+    backgroundColor: COLOR_CONST,
+    paddingHorizontal: 25,
+    paddingVertical: 7,
+    borderRadius: 20,
+    marginTop: 5,
+    position: 'absolute',
+    top: FIRSTTEXT_CONST + DIFFERENCE_CONST * 4.5 - 12,
+    right: 20,
+
+    borderBottomWidth: BORDER_BOTTOM_WIDTH_CONST,
+  },
+  inputHeartConditionDetails: {
+    backgroundColor: 'white',
+    width: SCREEN_WIDTH,
+    paddingHorizontal: 10,
+    paddingVertical: 10,
+    borderRadius: 20,
+    marginTop: 5,
+    position: 'absolute',
+    top: FIRSTTEXT_CONST + DIFFERENCE_CONST * 5.5,
+    left: 15,
+    fontSize: 20,
+    borderBottomWidth: BORDER_BOTTOM_WIDTH_CONST,
   },
   asthmaSubText: {
     fontSize: 23,
-    fontWeight: "normal",
+    fontWeight: 'normal',
     position: 'absolute',
     left: 25,
-    top: FIRSTTEXT_CONST + DIFFERENCE_CONST*7.5,
+    top: FIRSTTEXT_CONST + DIFFERENCE_CONST * 7.5,
   },
-  inputAsthma:{
-        backgroundColor:COLOR_CONST,
-        paddingHorizontal:25,
-        paddingVertical:7,
-        borderRadius:20,
-        marginTop:5,
-        position: 'absolute',
-        top: FIRSTTEXT_CONST + DIFFERENCE_CONST*7.5 - 10,
-        right: 20,
-        borderBottomWidth: BORDER_BOTTOM_WIDTH_CONST,
-  },  
-  inputHeartCondition:{
-        backgroundColor:COLOR_CONST,
-        paddingHorizontal:25,
-        paddingVertical:7,
-        borderRadius:20,
-        marginTop:5,
-        position: 'absolute',
-        top: FIRSTTEXT_CONST + DIFFERENCE_CONST*4.5 - 12,
-        right: 20,
-        
-        borderBottomWidth: BORDER_BOTTOM_WIDTH_CONST,
+  inputAsthma: {
+    backgroundColor: COLOR_CONST,
+    paddingHorizontal: 25,
+    paddingVertical: 7,
+    borderRadius: 20,
+    marginTop: 5,
+    position: 'absolute',
+    top: FIRSTTEXT_CONST + DIFFERENCE_CONST * 7.5 - 10,
+    right: 20,
+    borderBottomWidth: BORDER_BOTTOM_WIDTH_CONST,
   },
-  inputHeartConditionDetails:{
-        backgroundColor:'white',
-        width: SCREEN_WIDTH,
-        paddingHorizontal:10,
-        paddingVertical:10,
-        borderRadius:20,
-        marginTop:5,
-        position: 'absolute',
-        top: FIRSTTEXT_CONST + DIFFERENCE_CONST*5.5,
-        left: 15,
-        fontSize: 20,
-        borderBottomWidth: BORDER_BOTTOM_WIDTH_CONST,
+  otherHealthCondition: {
+    backgroundColor: 'white',
+    color: 'grey',
+    width: SCREEN_WIDTH,
+    paddingHorizontal: 10,
+    paddingVertical: 10,
+    borderRadius: 20,
+    marginTop: 5,
+    position: 'absolute',
+    top: FIRSTTEXT_CONST + DIFFERENCE_CONST * 8.5,
+    left: 15,
+    fontSize: 20,
+    borderBottomWidth: BORDER_BOTTOM_WIDTH_CONST,
   },
-  otherHealthCondition:{
-        backgroundColor:'white',
-        color: 'grey',
-        width: SCREEN_WIDTH,
-        paddingHorizontal:10,
-        paddingVertical:10,
-        borderRadius:20,
-        marginTop:5,
-        position: 'absolute',
-        top: FIRSTTEXT_CONST + DIFFERENCE_CONST*8.5,
-        left: 15,
-        fontSize: 20,
-        borderBottomWidth: BORDER_BOTTOM_WIDTH_CONST,
+  saveButtonContainer: {
+    width: '20%',
+    position: 'absolute',
+    bottom: 30,
+    left: 260,
   },
-  saveButtonContainer:{
-        width:'65%',
-        position: 'absolute',
-        bottom: 30,
-        left: 250
-    },
-   button:{
-        width:'35%',
-        paddingHorizontal:10,
-        paddingVertical:10,
-        borderRadius:10,
-    },
-    buttonOutline:{
-        backgroundColor:'lightskyblue',
-        borderWidth:BORDER_BOTTOM_WIDTH_CONST,
-        alignItems: 'center',
-    },
-    buttonOutlineText:{
-        fontWeight:'700',
-        fontStyle: 'italic',
-        fontSize:20,   
-    },
 });
